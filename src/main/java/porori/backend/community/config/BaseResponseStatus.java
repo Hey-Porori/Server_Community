0e3d1c7746df3699a9e5a6239cbe69df8e6c4846
jjunjji <younggy123@gmail.com>
2023-07-18 00:36:27 +0900

    base config 설정

---
+++ src/main/java/porori/backend/community/config/BaseResponseStatus.java
@@ -0,0 +1,49 @@
+package porori.backend.community.config;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000 : 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+
+    /**
+     * 2000 : Request 오류
+     */
+
+
+    /**
+     * 3000 : Response 오류
+     */
+
+
+    /**
+     * 4000 : Database, Server 오류
+     */
+    DATABASE_ERROR(false, 4000, "데이터베이스 저장에 실패하였습니다.");
+
+
+    /**
+     * 5000 : 필요시 만들어서 쓰세요
+     */
+
+
+    /**
+     * 6000 : 필요시 만들어서 쓰세요
+     */
+
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) { //BaseResponseStatus 에서 각 해당하는 코드를 생성자로 맵핑
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
