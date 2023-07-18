d719ad9bd40db593cff211751a9b494034bb59eb
jjunjji <younggy123@gmail.com>
2023-07-19 02:32:01 +0900

    feat: Swagger 설정

--- src/main/java/porori/backend/community/controller/PostController.java
+++ src/main/java/porori/backend/community/controller/PostController.java
@@ -1,5 +1,7 @@
 package porori.backend.community.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import porori.backend.community.config.BaseResponse;
@@ -15,12 +17,14 @@ import porori.backend.community.service.UserService;
 @RequiredArgsConstructor
 @RequestMapping("/api/communities")
 @RestController
+@Api(tags = "Post API")
 public class PostController {
     private final PostService postService;
     private final UserService userService;
     private final AmazonS3Service amazonS3Service;
 
     //게시글 작성하기
+    @ApiOperation(value = "게시글 작성", notes = "게시글을 작성합니다.")
     @PostMapping("/post")
     public BaseResponse<PostResDto.PostContentRes> createPost(@RequestHeader("Authorization") String token, @RequestBody PostReqDto.PostContentReq postContent) {
         //토큰 유효 확인
@@ -35,6 +39,8 @@ public class PostController {
     }
 
     //Pre-Signed Url 발급받기
+    @ApiOperation(value = "Pre-Signed Url 발급", notes = "Pre-Signed Url을 발급합니다.")
+
     @GetMapping("/url")
     public BaseResponse<PostResDto.PreSignedUrlRes> getPreSignedUrl(@RequestHeader("Authorization") String token) {
         //토큰 유효 확인
