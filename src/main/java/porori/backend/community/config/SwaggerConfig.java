9d711354f07ea9e8a9c89cc04c64ccbe9890f9c6
jjunjji <younggy123@gmail.com>
2023-07-26 21:24:20 +0900

    fix: Swagger springdoc으로 변경

--- src/main/java/porori/backend/community/config/SwaggerConfig.java
+++ src/main/java/porori/backend/community/config/SwaggerConfig.java
@@ -1,75 +1,32 @@
 package porori.backend.community.config;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Getter;
-import lombok.Setter;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
+@SecurityScheme(
+        type = SecuritySchemeType.APIKEY, in = SecuritySchemeIn.HEADER,
+        name = "Authorization", description = "Authorization"
+)
+@OpenAPIDefinition(security = { @SecurityRequirement(name = "Authorization") })
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
 
-    @Getter
-    @Setter
-    @ApiModel
-    static class Page {
-        @ApiModelProperty(value = "페이지 번호(0..N)")
-        private Integer page;
+    public OpenAPI openAPI() {
 
-        @ApiModelProperty(value = "페이지 크기", allowableValues="range[0, 100]")
-        private Integer size;
+        Info info = new Info()
+                .version("1.0.0")
+                .title("Porori_Community Rest API Documentation");
 
-        @ApiModelProperty(value = "정렬(사용법: 컬럼명,ASC|DESC)")
-        private List<String> sort;
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Porori_Community Rest API Documentation")
-                .version("1.0")
-                .build();
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .build();
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("Authorization", "Authorization", "header");
+        return new OpenAPI()
+                .info(info);
     }
 }
--- src/main/java/porori/backend/community/controller/PostController.java
+++ src/main/java/porori/backend/community/controller/PostController.java
@@ -1,54 +1,37 @@
 package porori.backend.community.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import porori.backend.community.config.BaseResponse;
-import porori.backend.community.config.exception.user.TokenException;
 import porori.backend.community.dto.PostReqDto;
 import porori.backend.community.dto.PostResDto;
 import porori.backend.community.service.AmazonS3Service;
 import porori.backend.community.service.PostService;
-import porori.backend.community.service.UserService;
-
 
 
 @RequiredArgsConstructor
 @RequestMapping("/api/communities")
 @RestController
-@Api(tags = "Post API")
+@Tag(name = "Post API", description = "게시글 API")
 public class PostController {
     private final PostService postService;
-    private final UserService userService;
     private final AmazonS3Service amazonS3Service;
 
     //게시글 작성하기
-    @ApiOperation(value = "게시글 작성", notes = "게시글을 작성합니다.")
+    @Operation(summary = "게시글 작성", description = "게시글을 작성합니다.")
     @PostMapping("/post")
     public BaseResponse<PostResDto.PostContentRes> createPost(@RequestHeader("Authorization") String token, @RequestBody PostReqDto.PostContentReq postContent) {
-        //토큰 유효 확인
-        if(!userService.validationToken(token)){
-            throw new TokenException();
-        }
-
-        Long userId = userService.getUserId(token);
-
-        PostResDto.PostContentRes postContentRes = postService.createPost(userId, postContent);
-        return new BaseResponse<>(postContentRes);
+        return new BaseResponse<>(postService.createPost(token, postContent));
     }
 
     //Pre-Signed Url 발급받기
-    @ApiOperation(value = "Pre-Signed Url 발급", notes = "Pre-Signed Url을 발급합니다.")
-
+    @Operation(summary = "Pre-Signed Url 발급", description = "Pre-Signed Url을 발급합니다.")
     @GetMapping("/url")
     public BaseResponse<PostResDto.PreSignedUrlRes> getPreSignedUrl(@RequestHeader("Authorization") String token) {
-        //토큰 유효 확인
-        if(!userService.validationToken(token)){
-            throw new TokenException();
-        }
-
-        PostResDto.PreSignedUrlRes urlRes = amazonS3Service.getPreSignedUrl();
-        return new BaseResponse<>(urlRes);
+        return new BaseResponse<>(amazonS3Service.getPreSignedUrl(token));
     }
+
+
 }
