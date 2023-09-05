7986bb8fc4974054aeaa08f1cfae79a1100454e3
jiyoon <95969941+jjunjji@users.noreply.github.com>
2023-09-05 18:45:41 +0900

    fix: User 서버 request 변경에 따른 수정사항 반영

--- src/main/java/porori/backend/community/service/UserService.java
+++ src/main/java/porori/backend/community/service/UserService.java
@@ -3,6 +3,7 @@ package porori.backend.community.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import porori.backend.community.config.BaseResponse;
@@ -11,10 +12,11 @@ import porori.backend.community.config.exception.user.UserBadGateWayException;
 import porori.backend.community.dto.UserReqDto.UserIdListReq;
 import porori.backend.community.dto.UserResDto.UserInfo;
 import porori.backend.community.dto.UserResDto.CommunityUserInfo;
-import porori.backend.community.dto.UserReqDto.AccessTokenReq;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -24,30 +26,19 @@ public class UserService {
     private static final String LOG_FORMAT = "Method : {}";
 
     public Long getUserId(String token) {
-        String[] splitToken = token.split(" ");
-
-        AccessTokenReq accessTokenReq = AccessTokenReq.builder()
-                .accessToken(splitToken[1])
-                .build();
-
-        return sendTokenMeRequest(token, accessTokenReq).getUserId();
+        return sendTokenMeRequest(token).getUserId();
     }
 
-    private UserInfo sendTokenMeRequest(String token, AccessTokenReq accessTokenReq) {
-            return webClient.post()
+    private UserInfo sendTokenMeRequest(String token) {
+            return webClient.get()
                     .uri("/token/me")
                     .header("Authorization", token)
-                    .bodyValue(accessTokenReq)
                     .retrieve()
+                    .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new TokenException()))
+                    .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new UserBadGateWayException()))
                     .bodyToMono((new ParameterizedTypeReference<BaseResponse<UserInfo>>() {
                     }))
-                    .map(response->{
-                        if (response.getStatusCode() == 200) {
-                            return response.getData();
-                        } else {
-                            throw new UserBadGateWayException();
-                        }
-                    })
+                    .map(BaseResponse::getData)
                     .block();
     }
 
@@ -67,21 +58,20 @@ public class UserService {
     }
 
     public List<CommunityUserInfo> sendCommunitiesInfoRequest(String token, UserIdListReq userIdList) {
-        return webClient.post()
-                .uri("/communities/info")
+        String param = userIdList.getUserIdList().stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+        log.warn(LOG_FORMAT, param);
+
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/communities/info").queryParam("userIdList", param).build())
                 .header("Authorization", token)
-                .bodyValue(userIdList)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new TokenException()))
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new UserBadGateWayException()))
                 .bodyToMono((new ParameterizedTypeReference<BaseResponse<Map<String, List<CommunityUserInfo>>>>() {
                 }))
-                .map(response->{
-                    if (response.getStatusCode() == 200) {
-                        log.warn("communityUserInfoBlocks : {}", "완");
-                        return response.getData().get("communityUserInfoBlocks");
-                    } else {
-                        throw new UserBadGateWayException();
-                    }
-                })
+                .map(response->response.getData().get("communityUserInfoBlocks"))
                 .block();
     }
 
